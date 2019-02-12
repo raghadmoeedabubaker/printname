e378cbb29e99fb1d8c9f0d3031d11c71d4cb212e
MIDITECH <MIDITECH@172.19.200.240>
2019-02-12 16:32:03 +0300

    new commit

---
+++ software/src/software/printname.java
@@ -0,0 +1,11 @@
+package software;
+
+public class printname {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Raghad Abu Baker");
+
+	}
+
+}
